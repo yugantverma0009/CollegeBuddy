4751a784db81c1d9b303b3cb98da93986440b1f7
yugant verma <yugantverma8448@gmail.com>
2026-04-28 00:05:41 +0530

    Proxy Vercel frontend API to Render backend

--- public/js/config.js
+++ public/js/config.js
@@ -3,4 +3,4 @@ window.CB_API_BASE =
   localStorage.getItem('cb_api_base') ||
   (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:5000/api'
-    : 'https://collegebuddy-backend.onrender.com/api');
+    : `${window.location.origin}/api`);
